a7426ead162dabd9aaebd6cab20dad15411d0d8f
harumu <rushia0901@gmail.com>
2025-05-23 00:10:14 +0900

    create-rayout

--- src/components/plots/PlotsList.tsx
+++ src/components/plots/PlotsList.tsx
@@ -11,7 +11,7 @@ const PlotsList = ( { plots } : PlotsListProps) => {
 
     
     return (
-        <ul className='flex flex-around justify-center flex-wrap m-5 max-w-content'>
+        <ul className='flex flex-wrap justify-center items-start  m-5 mb-20 max-w-content  shadow-md rounded-4xl'>
             {plots.map((plot) => (
                 <PlotDetail key={plot.id} plot = {plot}/>
             ))}
