9051d29cb70ffef79377507d31ccc3a765be3262
harumu <rushia0901@gmail.com>
2025-05-24 20:59:27 +0900

    タグ検索の追加

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Plot } from '@/types'
 import PlotsList from '../plots/PlotsList';
+import TagSearch from './TagSearch';
 
 interface SearchProps {
   plots: Plot[];
@@ -24,6 +25,10 @@ const Search = ({ plots }: SearchProps) => {
         placeholder="検索..."
         className="w-full px-4 py-2 border rounded"
       />
+      <div className='flex items-center'>
+        <p className='text-sm mr-2'>ヒント :</p>
+      <TagSearch plots={plots} setSearchQuery={setSearchQuery}/>
+      </div>
       {searchQuery && (
         <div className="mt-2">
           <PlotsList plots={filteredPlots}/>
